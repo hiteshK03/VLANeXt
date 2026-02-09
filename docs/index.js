01cdd334537e369bdde9f216d17159ec9cd8cd7e
DravenALG <wuxiaoming.alg@gmail.com>
2026-02-09 14:39:15 +0000

    info

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const roadmapImg = document.getElementById('roadmap-video');
+  if (roadmapImg) {
+    const gifUrl = roadmapImg.getAttribute('data-gif');
+    if (gifUrl) {
+      const img = new Image();
+      img.onload = function() {
+        roadmapImg.src = gifUrl;
+      };
+      img.src = gifUrl;
+    }
+  }
+});
 
