ea6016e6afdef1d3f9fa62d513d47c50949ccc62
DravenALG <wuxiaoming.alg@gmail.com>
2026-03-02 12:19:10 +0000

    info

--- src/datasets/libero_act.py
+++ src/datasets/libero_act.py
@@ -248,8 +248,8 @@ if __name__ == "__main__":
     # Configuration
     BASE_DIR = "/data/NTU_slab/draven/data/LIBERO_modified"
     SUITES = [
-        # "libero_spatial",
-        "libero_object",
+        "libero_spatial",
+        # "libero_object",
         # "libero_goal",
         # "libero_10",
     ]
